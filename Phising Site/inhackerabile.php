fa56590e7af39cb402d0bd666e69b8d511506b89
Error404 <88632271+not1m3@users.noreply.github.com>
2022-05-25 21:33:03 +0200

    Add files via upload

---
+++ Phising Site/inhackerabile.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1><b>PASSWORD INHACKERABILE</b></h1>
+    <img src="inhackerabile.jpg" alt="hack" width="50%" height="50%"> 
+  </body> 
+</html>
---
+++ Phising Site/ridi.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <img src="eh_volevi.jpg" alt="zeb" width="100%">
+  </body> 
+</html>
